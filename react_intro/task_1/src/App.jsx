d86cda4a9e8926dff6f09a3b8f532b0b65bd335d
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-03-12 15:17:13 +0100

    added

--- react_intro/task_1/src/App.jsx
+++ react_intro/task_1/src/App.jsx
@@ -5,27 +5,24 @@ import Notifications from "./Notifications";
 import './App.css'
 
 const App = () => {
-  // eslint-disable-next-line no-unused-vars
-  const date = new Date()
-
 
   return (
     <>
       <div className="root-notifications">
         <Notifications />
       </div>
+
       <div className='App-header'>
         <img src={logo} alt="holberton logo" />
         <h1 style={{ color:'#e1003c'}}>School dashboard</h1>
       </div>
+
       <div className='App-body'>
-        
-        <p >Login to access the full dashboard</p>
+        <p>Login to access the full dashboard</p>
       </div>
 
       <div className='App-footer'>
-
-        <p >Copyright {getCurrentYear()} - {getFooterCopy(false)}</p>
+        <p>Copyright {getCurrentYear()} - {getFooterCopy(false)}</p>
       </div>
     </>
   )
