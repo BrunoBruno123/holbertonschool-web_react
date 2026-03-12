297562ebdea9eade2eee3fa8eefc1f7d71a984a9
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-03-12 11:03:54 +0100

    changes

---
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -0,0 +1,20 @@
+/* eslint-env jest */
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+
+test("renders h1 with School Dashboard text",  () => {
+    render(<App/>)
+    expect(screen.getByRole('heading')).toHaveTextContent(/School dashboard/i);
+})
+
+test("renders correct text in body and footer paragraphs", () => {
+    render(<App />)
+    expect(screen.getByText(/Login to access the full dashboard/i));
+    expect(screen.getByText(/Copyright/i));
+})
+test("renders an image", () => {
+    render(<App />);
+    const image = screen.getByAltText(/holberton logo/i);
+    expect(image).toBeInTheDocument();
+});
