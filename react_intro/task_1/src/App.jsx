51e03455f4417eaaec60f77b7ce527efe51f98ea
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-03-12 14:24:39 +0100

    changes

--- react_intro/task_1/src/App.jsx
+++ react_intro/task_1/src/App.jsx
@@ -1,30 +1,25 @@
-import logo from './assets/holberton-logo.jpg';
-import { getCurrentYear, getFooterCopy } from "./utils";
-import Notifications from "./Notifications";
-
+import holbertonLogo from './assets/holberton-logo.jpg'
+import Notifications from './Notifications'
 import './App.css'
 
-const App = () => {
-  const date = new Date()
-
-
+function App() {
   return (
     <>
       <div className="root-notifications">
         <Notifications />
       </div>
-      <div className='App-header'>
-        <img src={logo} alt="holberton logo" />
-        <h1 style={{ color:'#e1003c'}}>School dashboard</h1>
-      </div>
-      <div className='App-body'>
-        
-        <p >Login to access the full dashboard</p>
+
+      <div className="App-header">
+        <img src={holbertonLogo} alt="holberton logo" />
+        <h1 style={{ color: "#e1003c" }}>School dashboard</h1>
       </div>
 
-      <div className='App-footer'>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
 
-        <p >Copyright {getCurrentYear()} - {getFooterCopy(false)}</p>
+      <div className="App-footer">
+        <p>Copyright {new Date().getFullYear()} - holberton School</p>
       </div>
     </>
   )
