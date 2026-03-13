c141884c686a4ff5d46690e938071c4f81cba1c1
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-03-13 10:59:40 +0100

    changes

--- react_intro/task_3/dashboard/src/Notifications.jsx
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -3,27 +3,34 @@ import close from './assets/close-button.png'
 import { getLatestNotification } from './utils'
 
 const Notifications = () => {
-  return <div className="notification-items   ">
+  return (
+    <div className="notification-items">
+      <p>Here is the list of notifications</p>
 
-    <p>Here is the list of notifications</p>
-    <ul>
-      <li data-priority="default">
-        New course available
-      </li>
-      <li data-priority="urgent">
-        New resume available
-      </li>
-      <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}>
+      <ul>
+        <li data-priority="default">New course available</li>
 
-      </li>
+        <li data-priority="urgent">New resume available</li>
 
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
 
-    </ul>
-    <button aria-label='Close' style={{ position: "absolute", top: "10px", right: "10px" }} onClick={() => { console.log('Close button has been clicked') }} >
-
-      <img src={close} style={{ width: '30px', height: '30px' }} />
-    </button>
-
-  </div>
+      <button
+        aria-label="Close"
+        style={{ position: "absolute", top: "10px", right: "10px" }}
+        onClick={() => console.log("Close button has been clicked")}
+      >
+        <img
+          src={close}
+          alt="close"
+          style={{ width: "30px", height: "30px" }}
+        />
+      </button>
+    </div>
+  )
 }
-export default Notifications;
+
+export default Notifications
