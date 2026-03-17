b5a41850bb35d1aa7ce73e7c34dac7aa47ab90da
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-03-17 11:14:28 +0100

    changes

---
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import Notifications from "../Notifications/Notifications";
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+      <Header />
+      <Login />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default App;
