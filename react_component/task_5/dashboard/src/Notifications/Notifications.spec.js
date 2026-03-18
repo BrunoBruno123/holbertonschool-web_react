e0604f28956ca97f441d73b64ec2501a2f18bcd7
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-03-18 14:38:50 +0100

    changes

--- react_component/task_5/dashboard/src/Notifications/Notifications.spec.js
+++ react_component/task_5/dashboard/src/Notifications/Notifications.spec.js
@@ -27,3 +27,39 @@ test('clicking notification logs correct message', () => {
 
   consoleSpy.mockRestore();
 });
+
+test('Notifications does not re-render if notifications length stays the same', () => {
+  const { rerender, getByText } = render(
+    <Notifications displayDrawer={true} notifications={notifications} />
+  );
+
+  const sameNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+  ];
+
+  rerender(
+    <Notifications displayDrawer={true} notifications={sameNotifications} />
+  );
+
+  expect(getByText('New course available')).toBeInTheDocument();
+  expect(getByText('New resume available')).toBeInTheDocument();
+});
+
+test('Notifications re-renders when notifications length changes', () => {
+  const { rerender, queryByText } = render(
+    <Notifications displayDrawer={true} notifications={notifications} />
+  );
+
+  const moreNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', value: 'New deadline urgent' },
+  ];
+
+  rerender(
+    <Notifications displayDrawer={true} notifications={moreNotifications} />
+  );
+
+  expect(queryByText('New deadline urgent')).toBeInTheDocument();
+});
