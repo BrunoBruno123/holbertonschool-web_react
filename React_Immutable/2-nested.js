5c1925a637d8b892f37f912c3ca101c918679758
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-04-20 11:10:58 +0200

    changes

--- React_Immutable/2-nested.js
+++ React_Immutable/2-nested.js
@@ -1,7 +1,6 @@
+/* eslint-disable */
 import { fromJS } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-    // const myKey = array.find((el) => { return el === 'first' });
-    //return fromJS(object).getIn(array)
-    const immutableObj = fromJS(object);
-    return immutableObj.getIn(array);
+  return fromJS(object).getIn(array);
 }
